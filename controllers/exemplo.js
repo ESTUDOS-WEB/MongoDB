c8331fee81eb1148b56f9d0b4098f1c0aaba1857
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2023-10-19 18:28:31 -0300

    Criando metodo: Inserir (CRUD)

--- controllers/exemplo.js
+++ controllers/exemplo.js
@@ -1,12 +1,10 @@
-import { conectar, desconectar } from "../utils/dataBase";
-
 const ControllerExemplo = {
   collection: {
     async criar(conexao, nomeCollection = "") {
       try {
-        if (!conexao) throw new Error("Necessario informamar conexão.");
+        if (!conexao) throw new Error("Necessario informar conexão.");
         if (!nomeCollection)
-          throw new Error("Necessario informamar nome para coleção.");
+          throw new Error("Necessario informar nome para coleção.");
 
         // Executa a operação
         await conexao.banco.createCollection(nomeCollection, {});
@@ -21,15 +19,35 @@ const ControllerExemplo = {
 
     async deletar(conexao, nomeCollection = "") {
       try {
-        if (!conexao) throw new Error("Necessario informamar conexão.");
+        if (!conexao) throw new Error("Necessario informar conexão.");
         if (!nomeCollection)
-          throw new Error("Necessario informamar nome para coleção.");
+          throw new Error("Necessario informar nome para coleção.");
 
         // Executa a operação
         await conexao.banco.dropCollection(nomeCollection);
 
         // Mensagem confirmando execçaõ da operação
         console.log(`Collection ${nomeCollection} deletada.`);
+      } catch (error) {
+        // Erro ao tentar executar a operação
+        console.error(error.message);
+      }
+    },
+  },
+
+  crud: {
+    async inserir(conexao, nomeCollection, dados) {
+      try {
+        if (!conexao) throw new Error("Necessario informar conexão.");
+        if (!nomeCollection)
+          throw new Error("Necessario informar nome para coleção.");
+        if (!dados) throw new Error("Necessario informar dodos.");
+
+        // Executa a operação
+        await conexao.banco.collection(nomeCollection).insertOne(dados);
+
+        // Mensagem confirmando execçaõ da operação
+        console.log(`Registro adicionado na coleção ${nomeCollection}.`);
       } catch (error) {
         // Erro ao tentar executar a operação
         console.error(error.message);
