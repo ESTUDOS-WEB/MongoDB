df4687fcec74714540f83b27c62a82143cbe5eac
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2023-10-19 07:18:33 -0300

    Restruturando código de funções

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 import { ControllerExemplo } from "./controllers/exemplo";
+import { conectar, desconectar } from "./utils/dataBase";
 
-// ControllerExemplo.collection.criar("minha_nova_colecao");
-// ControllerExemplo.collection.deletar("minha_nova_colecao");
+conectar()
+    .then( async (conexao) => {
+    
+        await ControllerExemplo.collection.criar(conexao, "minha_nova_colecao");
+        await ControllerExemplo.collection.deletar(conexao, "minha_nova_colecao");
+        desconectar(conexao.conexao);
+
+    })
+    .catch(erro =>
+
+        // Erro ao tentar se conectar 
+        console.error(erro)
+    
+    );
